383cd5ae2a7a92c06e2563fcd63e4d3fadd49667
joachimhuber <joachim.huber@bbv.ch>
2015-09-29 10:10:14 +0200

    transfer

--- src/test/java/com/bbv/javadojo/optional/ArgumentParserTest.java
+++ src/test/java/com/bbv/javadojo/optional/ArgumentParserTest.java
@@ -1,9 +1,23 @@
 package com.bbv.javadojo.optional;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 public class ArgumentParserTest {
     @Test
     public void testCreateWithNoSchemaOrArguments() throws Exception {
         final ArgumentParser args = new ArgumentParser("", new String[0]);
+    }
+
+    @Test
+        public void parse_booleanSchemaBooleanPresent() {
+        final ArgumentParser args = new ArgumentParser("x", new String[] { "-x" });
+        assertEquals(true, args.getBoolean("x"));
+    }
+
+    @Test
+    public void parse_stringSchemaStringPresent() {
+        final ArgumentParser args = new ArgumentParser("x*", new String[] { "-x te" });
+        assertEquals("te", args.getString("x"));
     }
 }
